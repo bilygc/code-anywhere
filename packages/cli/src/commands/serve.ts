52abed1d518da86767825b8e957194ec643ddf5f
bilygc <licbgomez@gmail.com>
2022-08-30 22:36:37 -0600

    rename npm organization

--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import { Command } from "commander";
-import { serve } from "@reactanywhere/local-api";
+import { serve } from "@code-anywhere/local-api";
 
 const isProduction = process.env.NODE_ENV === "production";
 
